08512ddfcfbc80411e8afe5a580984424215b400
Jay Callicott <jrcallicott@gmail.com>
2026-03-08 08:42:45 -0500

    fix: mark header-dependent routes as dynamic

--- app/grants/page.tsx
+++ app/grants/page.tsx
@@ -7,6 +7,7 @@ import Header from '../components/Header'
 import GrantCard from '../components/GrantCard'
 
 export const revalidate = 3600
+export const dynamic = 'force-dynamic'
 
 export const metadata: Metadata = {
   title: 'Grants | Evergreen Community Foundation',
